c1c7e9198ead5fec033cf1320c823eb257dc3150
xiaojin <33309882+xiaojin525@users.noreply.github.com>
2019-08-16 11:38:07 +0800

    Update uploadchart.go

--- uploadchart.go
+++ uploadchart.go
@@ -4,6 +4,7 @@ import (
 	"encoding/base64"
 	"fmt"
 	"github.com/ddliu/go-httpclient"
+	"github.com/mholt/archiver"
 	"github.com/urfave/cli"
 	"log"
 	"os"
@@ -12,7 +13,6 @@ import (
 const (
 	USERAGENT        = "lujin httpclient"
 	TIMEOUT          = 30
-	SERVER           = "https://harbor.wise-paas.io/api/chartrepo/tekton/charts"
 )
 
 func postfile (url string, filename string, username string, password string) (code int, text string) {
@@ -43,26 +43,50 @@ func postfile (url string, filename string, username string, password string) (c
 	return res.StatusCode, text
 }
 
+
+func IsDir(path string) bool {
+	s, err := os.Stat(path)
+	if err != nil {
+		return false
+	}
+	return s.IsDir()
+}
+
+
+
+func tgzfile(file string, filetgzname string) error {
+	if IsDir(file)  {
+		// archive format is determined by file extension
+		err := archiver.Archive([]string{file}, filetgzname)
+		if err != nil {
+    		return err
+		}
+	}else{
+		fmt.Printf("%s is not a dir\n", file)
+	}
+	return nil
+
+}
+
+
 func main() {
 	app := cli.NewApp()
 	app.Name = "UploadChart"
 	app.Usage = "upload chart .tgz to harbor"
-	app.Version = "0.0.1"
+	app.Version = "0.0.2"
 	app.Flags = []cli.Flag{
 		cli.StringFlag{
 			Name:  "file, f",
 			Usage: "Load helm chart .tgz file",
-			Value: "test2.tgz",
+			Value: "test.tgz",
 		},
 		cli.StringFlag{
 			Name:  "username, u",
 			Usage: "username for harbor",
-			Value: "admin",
 		},
 		cli.StringFlag{
 			Name:  "password, p",
 			Usage: "passworld for harbor",
-			Value: "admin",
 		},
 		cli.StringFlag{
 			Name:  "harborurl, url",
@@ -75,9 +99,31 @@ func main() {
 		harborurl := c.String("harborurl")
 		username := c.String("username")
 		password := c.String("password")
-		code, text := postfile(harborurl, file, username, password)
+		//fmt.Println(file,harborurl,username,password)
+
+		var filetgzname string
+
+		if IsDir(file) {
+			filetgzname = file + ".tgz"
+		}else{
+			filetgzname = file
+		}
+
+
+		if file != "" {
+			err := tgzfile(file,filetgzname)
+			if err != nil {
+				fmt.Println(err)
+			}
+			code, text := postfile(harborurl, filetgzname, username, password)
+			fmt.Printf("code: %d \nresponse: %s\n", code,text)
+		}else {
+			//time.Sleep(time.Second * 10)
+			fmt.Printf("--help, -h            show help \n")
+			//time.Sleep(time.Minute * 10)
+		}
 		//fmt.Println(code,text)
-		fmt.Printf("code: %d \nresponse: %s\n", code,text)
+
 		return nil
 	}
 
